d85ad93db4cca2b459c13cb91f652e32c73e9236
Pavel Kuzmin <mpakunderscore@gmail.com>
2016-01-23 01:31:42 +0300

    Menu

--- public/javascripts/edflowController.js
+++ public/javascripts/edflowController.js
@@ -27,6 +27,8 @@ edflow
 
                     $scope.pages = data.pages;
                     $scope.subCategories = data.subCategories;
+
+                    $scope.moveMenu();
                 })
                 .error(function (e) {
                     console.log(e);
@@ -45,6 +47,39 @@ edflow
             else
                 $scope.select($scope.selectedCategories.pop());
 
+        }
+
+        var getLongest = function(arr) {
+
+            var length = 0;
+            var longest;
+
+            for (var i = 0; i < arr.length; i++){
+
+                if (arr[i].title.length > length) {
+
+                    var length = arr[i].title.length;
+                    longest = arr[i];
+                }
+            }
+
+            return longest;
+        }
+
+        $scope.moveMenu = function() {
+
+            //var biggest = $scope.subCategories.reduce(function (a, b) { return a.length < b.length ? a : b; });
+            //var longest = $scope.subCategories.sort(function (a, b) { return b.length - a.length; })[0];
+
+            var longest = getLongest($scope.subCategories);
+            //console.log(longest.title.length);
+
+            var menu = longest.title.length * 8;
+            console.log(longest.title + " " + longest.title.length);
+
+            $scope.mWidth = (45 + menu) + "px";
+            $scope.cWidth = "calc(100% - " + $scope.mWidth + ")";
+            //console.log(document.getElementById("menu").offsetWidth)
         }
     })
 
